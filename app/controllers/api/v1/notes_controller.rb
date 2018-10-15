9420de8458f38ea710f0ea616aac15ccc45803ab
jessebillard <jessebillard@mac.com>
2018-10-15 00:37:33 -0400

    adds CORS configuration, to gemfile and config app file

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -5,6 +5,13 @@ class Api::V1::NotesController < ApplicationController
   end
 
   def create
+    @note = Note.new(note_params)
+    if @note.save
+      render json: @note
+    else
+      render json: error_message
+    end
+    byebug
   end
 
   def update
@@ -12,4 +19,15 @@ class Api::V1::NotesController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:title, :description)
+  end
+
+  def error_message
+    {status: "error", code: 400, message: "This request cannot be completed"}
+  end
+
 end
